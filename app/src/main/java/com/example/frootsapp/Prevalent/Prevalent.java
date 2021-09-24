86b24aa07598c5b8bff6776fb9ca62cfcde6ccfd
Thilokanjalee <it20087142@my.sliit.lk>
2021-09-25 01:07:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/frootsapp/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.frootsapp.Prevalent;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class Prevalent {
+    public static FirebaseUser CurrentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
